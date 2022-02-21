953f38074870c6e9ec97808a3e3ef633ecd3d4c6
AntonioAOrtiz <aaortiz.alumno@iesrafaelalberti.es>
2022-02-21 14:15:23 +0100

    tipo poppins, intento de usar esas fuentes, estilos header

--- dist/style.ff1c4cab.js
+++ dist/style.ff1c4cab.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"C:\\Users\\Samsung\\Desktop\\Diseño de Interfaces Web\\mortal-parkour\\src\\fonts\\Poppins-Regular.ttf":[["Poppins-Regular.afe0ffc8.ttf","fonts/Poppins-Regular.ttf"],"fonts/Poppins-Regular.ttf"],"C:\\Users\\Samsung\\Desktop\\Diseño de Interfaces Web\\mortal-parkour\\src\\fonts\\blowbrush.ttf":[["blowbrush.105b11c9.ttf","fonts/blowbrush.ttf"],"fonts/blowbrush.ttf"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55919" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55529" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
