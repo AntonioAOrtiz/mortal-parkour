98ce3670479633e51168e5a66d2b327b382e0cda
AntonioAOrtiz <aaortiz.alumno@iesrafaelalberti.es>
2022-03-01 18:03:20 +0100

    carruseles con imagenes responsive

---
+++ src/js/carrusel2.js
@@ -0,0 +1,19 @@
+
+const grande2 = document.querySelector('.grande2');
+const punto2 = document.querySelectorAll('.punto2')
+
+
+punto2.forEach (( cadaPunto, i)=> {
+    punto2[i].addEventListener('click',()=>{
+
+        let posicion = i;
+        let operacion = posicion * -33;
+
+        grande2.style.transform = `translateX(${ operacion }%)`;
+
+        punto2.forEach( (cadaPunto, i)=>{
+            punto2[i].classList.remove('activo2');
+        })
+        punto2[i].classList.add('activo2');
+    })
+})
